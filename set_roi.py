23a125544feb2e13b07e2aef61febc1260d4e88e
Eric <eric.emj.johnson@gmail.com>
2022-08-19 15:09:30 -0500

    added more examples

---
+++ set_roi.py
@@ -0,0 +1,15 @@
+from pymavlink import mavutil
+
+# Start a connection listening to a UDP port
+the_connection = mavutil.mavlink_connection('udpin:localhost:14551')
+
+# Wait for the first heartbeat
+#   This sets the system and component ID of remote system for the link
+the_connection.wait_heartbeat()
+
+the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component,
+                                     mavutil.mavlink.MAV_CMD_DO_SET_ROI, 0, 0, 0, 0, 0, -35.3631589, 149.1656256, 0)
+msg = the_connection.recv_match(type='COMMAND_ACK', blocking=True)
+print(msg)
+
+print(mavutil.mavlink.enums['MAV_RESULT'][msg.result].description)
