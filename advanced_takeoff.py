e8f56b894076fd67a3666f5a8cd8a26668a826de
Eric <eric.emj.johnson@gmail.com>
2023-02-27 22:11:55 -0600

    started cleaning up

--- advanced_takeoff.py
+++ advanced_takeoff.py
@@ -2,19 +2,47 @@
 from pymavlink import mavutil
 import time
 import sys
+import argparse
 
+
+def recv_match_sysid(sysid, con, type, timeout=3):
+    """recv_match_sysid receives a mavlink message from a specific sysid
+
+    Args:
+        sysid (int): sysid to listen for
+        con (mavutil): mavutil connection object
+    """    
+    time_start = time.time()
+    while time.time() - time_start < timeout:
+        msg = con.recv_match(type=type, blocking=True)
+        if msg.get_srcSystem() == sysid:
+            return msg
+    return {}
 
 def get_version_info(con):
-    msg = con.recv_match(type='HEARTBEAT', blocking=True, timeout=3)
-    autopilot_info = mavutil.mavlink.enums['MAV_AUTOPILOT'][msg.autopilot].description
+    """get_version_info gets the version info from the autopilot
+
+    Args:
+        con (mavutil): mavutil connection object
+    """    
+    msg = recv_match_sysid(con.target_system, con, 'HEARTBEAT', timeout=3)
+    print(msg)
+    print(msg.get_srcSystem())
     print(mavutil.mavlink.enums['MAV_AUTOPILOT'][msg.autopilot].description)
     print(mavutil.mavlink.enums['MAV_TYPE'][msg.type].description)
 
 
-def connect_to_sysid(connection_str, sysid, timeout=3):
+def connect_to_sysid(connection_str : str, sysid : int, timeout: float = 3) -> any:
+    """connect_to_sysid connects to a mavlink stream with a specific sysid
 
+    Args:
+        connection_str (str, optional): _description_. Defaults to 3)->mavutil.mavlink_connection(.
+
+    Returns:
+        _type_: _description_
+    """    
     time_start = time.time()
-    while time.time() - time_start < 3:
+    while time.time() - time_start < timeout:
         the_connection = mavutil.mavlink_connection(connection_str)
         the_connection.wait_heartbeat()
         print(
@@ -32,7 +60,7 @@ def ekf_pos_aiding(flags):
     ekf_flags = msg.flags
 
     for flag in flags:
-
+        print(flag)
         if ekf_flags & mavutil.mavlink.enums['EKF_STATUS_FLAGS'][flag]:
             continue
         else:
@@ -42,7 +70,12 @@ def ekf_pos_aiding(flags):
 
 
 if __name__ == "__main__":
-    con = connect_to_sysid('udpin:localhost:14551', 2)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--sysid", type=int, default=1)
+
+    args = parser.parse_args()
+
+    con = connect_to_sysid('udpin:localhost:14551', args.sysid)
     # Start a connection listening to a UDP port
 
     get_version_info(con)
