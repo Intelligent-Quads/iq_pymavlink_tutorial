a403012c7a952e63ceff6117f4cf66d23db9282a
Eric <eric.emj.johnson@gmail.com>
2023-06-04 12:13:48 -0700

    updated test setup to ensure to connect to sysid 1

--- unittests/test_all.py
+++ unittests/test_all.py
@@ -17,11 +17,31 @@ class TestAll(unittest.TestCase):
         self.simulator = SITLSimulator()
         conn_str = self.simulator.start()
         time.sleep(10)
-        self.mav_connection = mavutil.mavlink_connection(conn_str)
+        self.mav_connection = self.connect_to_sysid(conn_str, 1)
 
     def tearDown(self):
         print("Stopping simulator")
         self.simulator.stop()
+
+    def connect_to_sysid(self, connection_str : str, sysid : int, timeout: float = 3) -> any:
+        """connect_to_sysid connects to a mavlink stream with a specific sysid
+
+        Args:
+            connection_str (str, optional): _description_. Defaults to 3)->mavutil.mavlink_connection(.
+
+        Returns:
+            _type_: _description_
+        """    
+        time_start = time.time()
+        while time.time() - time_start < timeout:
+            the_connection = mavutil.mavlink_connection(connection_str)
+            the_connection.wait_heartbeat()
+            print(
+                f"Heartbeat from system system {the_connection.target_system} component {the_connection.target_component}")
+            if the_connection.target_system == sysid:
+                print(f"Now connected to SYSID {sysid}")
+                return the_connection
+
 
     def test_arm(self):
         result = arm(self.mav_connection, 1)
