d59c1f0f7ac584d12e2ae4bc41105e6395821938
Eric <eric.emj.johnson@gmail.com>
2023-07-26 22:41:09 -0700

    cleaned up utility functions

--- utilities/connect_to_sysid.py
+++ utilities/connect_to_sysid.py
@@ -1,22 +1,32 @@
-
 from pymavlink import mavutil
 import time
 
 def connect_to_sysid(connection_str : str, sysid : int, timeout: float = 3) -> any:
-    """connect_to_sysid connects to a mavlink stream with a specific sysid
+    """
+    connect_to_sysid connects to a mavlink stream with a specific sysid
 
     Args:
-        connection_str (str, optional): _description_. Defaults to 3)->mavutil.mavlink_connection(.
+        connection_str (str): String containing the connection information
+        sysid (int): The system id to connect to
+        timeout (float, optional): Maximum time to wait for the connection in seconds. Defaults to 3.
 
     Returns:
-        _type_: _description_
+        mavutil.mavlink_connection: Returns the connection object if connection is successful, 
+        else returns None after the timeout
     """    
+    the_connection = mavutil.mavlink_connection(connection_str)
     time_start = time.time()
+
     while time.time() - time_start < timeout:
-        the_connection = mavutil.mavlink_connection(connection_str)
-        the_connection.wait_heartbeat()
-        print(
-            f"Heartbeat from system system {the_connection.target_system} component {the_connection.target_component}")
-        if the_connection.target_system == sysid:
-            print(f"Now connected to SYSID {sysid}")
-            return the_connection
+        try:
+            the_connection.wait_heartbeat()
+            print(f"Heartbeat from system {the_connection.target_system} component {the_connection.target_component}")
+            if the_connection.target_system == sysid:
+                print(f"Now connected to SYSID {sysid}")
+                return the_connection
+        except Exception as e:
+            print(f"Error while waiting for heartbeat: {e}")
+            return None
+
+    print(f"Connection timeout after {timeout} seconds")
+    return None
